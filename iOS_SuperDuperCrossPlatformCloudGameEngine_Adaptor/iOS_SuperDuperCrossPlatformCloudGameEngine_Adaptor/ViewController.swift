9f040dc6fa351984555e0cb403a504a1502547c2
sstevenshang <sstevenshang@gmail.com>
2018-02-24 00:47:20 -0600

    Initial commit for iOS

---
+++ iOS_SuperDuperCrossPlatformCloudGameEngine_Adaptor/iOS_SuperDuperCrossPlatformCloudGameEngine_Adaptor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_SuperDuperCrossPlatformCloudGameEngine_Adaptor
+//
+//  Created by Steven Shang on 2/24/18.
+//  Copyright © 2018 sstevenshang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
